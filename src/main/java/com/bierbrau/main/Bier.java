711eab6b88c6e6de44aa0fa04ef7d54c426dca2e
HendrikStoeber <hendrik.stoeber@web.de>
2022-12-08 20:48:48 +0100

    Bier Konstruktor

---
+++ src/main/java/com/bierbrau/main/Bier.java
@@ -0,0 +1,20 @@
+package com.bierbrau.main;
+
+public class Bier {
+
+    private int stammwuerze;
+    private double farbe;
+    private double alkoholgehalt;
+    private String name;
+    private String sorte;
+
+
+    public Bier(int stammwuerze, int farbe, int alkoholgehalt, String name) {
+        this.stammwuerze = stammwuerze;
+        this.farbe = farbe;
+        this.alkoholgehalt = alkoholgehalt;
+        this.name = name;
+
+    }
+
+}
