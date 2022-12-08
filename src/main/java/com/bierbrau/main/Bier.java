3889f4927add3a98f09e95e06b554f5058c334ee
HendrikStoeber <hendrik.stoeber@web.de>
2022-12-08 20:55:37 +0100

    Bier Konstruktor

--- src/main/java/com/bierbrau/main/Bier.java
+++ src/main/java/com/bierbrau/main/Bier.java
@@ -9,12 +9,55 @@ public class Bier {
     private String sorte;
 
 
+
     public Bier(int stammwuerze, int farbe, int alkoholgehalt, String name) {
         this.stammwuerze = stammwuerze;
         this.farbe = farbe;
         this.alkoholgehalt = alkoholgehalt;
         this.name = name;
 
+
+    }
+
+
+    public int getStammwuerze() {
+        return stammwuerze;
+    }
+
+    public void setStammwuerze(int stammwuerze) {
+        this.stammwuerze = stammwuerze;
+    }
+
+    public double getFarbe() {
+        return farbe;
+    }
+
+    public void setFarbe(double farbe) {
+        this.farbe = farbe;
+    }
+
+    public double getAlkoholgehalt() {
+        return alkoholgehalt;
+    }
+
+    public void setAlkoholgehalt(double alkoholgehalt) {
+        this.alkoholgehalt = alkoholgehalt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSorte() {
+        return sorte;
+    }
+
+    public void setSorte(String sorte) {
+        this.sorte = sorte;
     }
 
 }
