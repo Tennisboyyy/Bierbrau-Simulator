24301ec9090a1fa3d0c80ceb6ec617a82a0bf61f
niMtAndoX <maxweber1603@gmail.com>
2022-12-27 22:31:54 +0100

    Maischen und FXML

--- src/main/java/com/bierbrau/game/abteilungen/Maischen.java
+++ src/main/java/com/bierbrau/game/abteilungen/Maischen.java
@@ -5,10 +5,19 @@ import java.io.Serializable;
 public class Maischen implements Serializable {
 
     public void erhitzen(){
+        int time = 0;
+        while(TopfaufBrenner = true){//Timer der hochgeht solange Topf über Brenner
+            time++;
+        }
+
     }
     public void malzHinzugabe(){
     }
     public void ruehren(){
+    }
+
+    public void warten(){
+
     }
 
 }
